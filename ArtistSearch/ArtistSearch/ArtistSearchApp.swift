3bb7addb7296ccfce1c3180bd70744018732cc9e
Przemyslaw Cackowski <cackowski.p@gmail.com>
2021-10-06 11:18:25 +0200

    initial commit hello world

---
+++ ArtistSearch/ArtistSearch/ArtistSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistSearchApp.swift
+//  ArtistSearch
+//
+//  Created by Przemyslaw Cackowski on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ArtistSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
